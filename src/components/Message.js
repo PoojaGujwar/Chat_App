323ce86f87c472ed50014ef9f34ec2f998ba287a
Pooja Gujwar <gujwarpooja@gmail.com>
2025-12-21 17:08:50 +0530

    Message UI Setup

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,34 +1,63 @@
 import "./message.css"
-const Message =({messages,user,onBack})=>{
-
-    const formateTime =(time)=>{
+const Message = ({ messages, user, onBack }) => {
+    let lastDate = ""
+    const formateTime = (time) => {
         const date = new Date(time);
-        console.log(date,messages)
-        return date.toLocaleDateString([],{
-            hour:"2-digit",
-            minute:"2-digit"
+        console.log(date, messages)
+        return date.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit"
         })
     }
-   
+    const formateDate = (dateStr) => {
+        const date = new Date(dateStr)
+        const today = new Date();
+        const yesterday = new Date();
+        yesterday.setDate(today.getDate() - 1)
+
+        if (date.toDateString() === today.toDateString()) {
+            return "Today"
+        }
+        if (date.toDateString() === yesterday.toDateString()) {
+            return "Yesterday"
+        }
+        return date.toLocaleDateString("en-IN", {
+            day: "2-digit",
+            month: "short",
+            year: "numeric"
+        })
+    }
+
     return (
         <>
-        <button onClick={onBack} setUser={user} className="back-btn">
-                    <i className="bi bi-arrow-left fs-3"></i>
-        </button>
-        <div className="message-list">
+            <button onClick={onBack} setUser={user} className="back-btn">
+                <i className="bi bi-arrow-left fs-3"></i>
+            </button>
+            <div className="message-list">
 
-            {messages.map((msg,index)=>(
-                <div
-                key={index}
-                 className={`message ${
-            msg.sender === user.username ? "sent" : "received"
-          }`}
-                >
-                   {msg.message}
-                   <div>{formateTime(msg.createdAt)}</div>
-                </div>
-            ))}
-        </div>
+                {messages.map((msg, index) => {
+                    const messageDate = new Date(msg.createdAt).toDateString();
+                    const showDate = messageDate != lastDate
+                    lastDate = messageDate
+                    return (
+                        <div key={index}>
+                            {showDate && <div className="date-separate">{formateDate(msg.createdAt)}</div>}
+                            <div className="message-row">
+                            <div
+
+                                className={`message ${msg.sender === user.username ? "sent" : "received"
+                                    }`}
+                            >
+                                <span>{msg.message}</span>
+                                <span className="time">{formateTime(msg.createdAt)}</span>
+                            </div>
+                            </div>
+                        </div>
+                    )
+
+
+                })}
+            </div>
         </>
     )
 }
